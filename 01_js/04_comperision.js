c84d2bcadaf7bfd4e092977817085d0be708fe19
yash0909221 <yashpurohit0909@gmail.com>
2026-03-06 09:03:51 +0000

    comperision

---
+++ 01_js/04_comperision.js
@@ -0,0 +1,18 @@
+console.log ("2" > 1)
+ // string convert to num 
+
+console.log (null > 0)
+console.log (null >= 0)
+
+// comperision and equal work diffrently
+
+console.log (undefined > 0)
+console.log (undefined >= 0)
+
+// undefined don't react to this trap and throw false 
+
+console.log ("1" == 1)
+console.log ("1" === 1)
+
+// === is used when you want to strictly check the values with its data types also
+
