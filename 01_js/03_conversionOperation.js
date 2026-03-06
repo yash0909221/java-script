087981fa18a792c388139638ba6b0783af81bd7f
yash0909221 <yashpurohit0909@gmail.com>
2026-03-06 08:45:49 +0000

    operators

---
+++ 01_js/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = 32
+
+console.log(typeof score)
+
+let valueInstring = String(score)
+console.log(typeof valueInstring)
+console.log(valueInstring)
+
+// "32" = 32
+// "32ab" = NaN
+// true = 1 ,false = 0 
+
+
+// ************ Operations ***************
+
+let value = 3 
+let valueNegative = -value
+console.log(valueNegative)
+
+console.log(2**2) // ** power functions
+
+let string = "yash"
+let string2 = "purohit"
+
+let sting3 = string + string2
+
+console.log(sting3);
