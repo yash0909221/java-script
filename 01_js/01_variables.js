88a93975f014046e0002059d94d9cc596bf69b86
yash0909221 <yashpurohit0909@gmail.com>
2026-03-06 07:59:23 +0000

    datatypes

---
+++ 01_js/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12
+let accountEmail = "yashpurohit0909@gmail.com"
+var accountpassword = "12344"
+accountcity = "delhi"
+
+
+//Avoid using var in real life projects because of its problems 
+
+console.log(accountId);
+console.table([accountId , accountEmail , accountpassword , accountcity]);
---
+++ 01_js/02_DataType.js
@@ -0,0 +1,21 @@
+"use Strict"; // used to force compiler to use newer js version
+
+// alert(3 + 3) // we are using node js not browser 
+
+let name = "yash"
+let age = 21
+let married = false
+
+// string
+// number
+// bigint
+// boolean
+// null = standalone value
+// symbol
+
+// object
+
+console.log (typeof name);
+
+// typeof null (object)
+// typeof undefined (undefined)
